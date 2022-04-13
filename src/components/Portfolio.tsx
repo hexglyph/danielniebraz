fd0a2d9f6126b9ee46fd21765b53b6a084faa90f
Daniel Niemietz Braz <dnbraz@prodam.sp.gov.br>
2022-04-13 18:51:56 -0300

    Added Vue link

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -34,6 +34,82 @@ export default function Portfolio(props) {
                         ring-1 p-2 my-2 mx-4 ring-slate-500`} target="_blank">Original Site</a>
                         </Link>
                     </div>
+
+                    <div className={`flex w-full md:w-1/6 flex-col`}>
+                        <h4 className={`text-base w-full text-left font-semibold 
+                    underline decoration-4 decoration-sky-500
+                    pl-4 pr-5 mb-4 
+                    rounded text-white`}>
+                            Convert Adobe XD to Website
+                        </h4>
+                        <p className={`pl-4`}>Adobe XD converted and ready to deploy.</p>
+                        <Link href="https://danielniebraz.dev/" passHref>
+                            <a className={`text-white hover:text-slate-50
+                        hover:bg-slate-50/10
+                        rounded-lg
+                        cursor-pointer
+                        ring-1 p-2 my-2 mx-4 ring-slate-500 `} target="_blank">Adobe XD File</a>
+                        </Link>
+                        <Link href="https://themechasnft.com/" passHref>
+                            <a className={`text-white hover:text-slate-50
+                        hover:bg-slate-50/10
+                        rounded-lg
+                        cursor-pointer
+                        ring-1 p-2 my-2 mx-4 ring-slate-500`} target="_blank">Website</a>
+                        </Link>
+                    </div>
+
+                    <div className={`flex w-full md:w-1/6 flex-col`}>
+                        <h4 className={`text-base w-full text-left font-semibold 
+                    underline decoration-4 decoration-sky-500
+                    pl-4 pr-5 mb-4 
+                    rounded text-white`}>
+                            Frontend Vue Project
+                        </h4>
+                        <p className={`pl-4`}>Site developed using Vue, TailwindCSS and Vuetify.</p>
+                        <Link href="https://vue.danielniebraz.dev/" passHref>
+                            <a className={`text-white hover:text-slate-50
+                        hover:bg-slate-50/10
+                        rounded-lg
+                        cursor-pointer
+                        ring-1 p-2 my-2 mx-4 ring-slate-500 `} target="_blank">Vue Project</a>
+                        </Link>
+                    </div>
+
+                    <div className={`flex w-full md:w-1/6 flex-col`}>
+                        <h4 className={`text-base w-full text-left font-semibold 
+                    underline decoration-4 decoration-sky-500
+                    pl-4 pr-5 mb-4 
+                    rounded text-white`}>
+                            TailwindCSS Project
+                        </h4>
+                        <p className={`pl-4`}>Site developed with TailwindCSS.</p>
+                        <Link href="https://danielniebraz.dev/" passHref>
+                            <a className={`text-white hover:text-slate-50
+                        hover:bg-slate-50/10
+                        rounded-lg
+                        cursor-pointer
+                        ring-1 p-2 my-2 mx-4 ring-slate-500 `} target="_blank">TailwindCSS Project</a>
+                        </Link>
+                    </div>
+
+                    <div className={`flex w-full md:w-1/6 flex-col`}>
+                        <h4 className={`text-base w-full text-left font-semibold 
+                    underline decoration-4 decoration-sky-500
+                    pl-4 pr-5 mb-4 
+                    rounded text-white`}>
+                            Flutter Project
+                        </h4>
+                        <p className={`pl-4`}>Flutter Project.</p>
+                        <Link href="https://danielniebraz.dev/" passHref>
+                            <a className={`text-white hover:text-slate-50
+                        hover:bg-slate-50/10
+                        rounded-lg
+                        cursor-pointer
+                        ring-1 p-2 my-2 mx-4 ring-slate-500 `} target="_blank">Flutter Project</a>
+                        </Link>
+                    </div>
+
                 </div>
 
             </div >
