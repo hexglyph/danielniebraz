a0d2fc8cb1d6e49cd05b74dd27ecef6acc51ee12
hexglyph <daniel.niebraz@gmail.com>
2022-05-15 22:39:57 -0300

    New portfolio items

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -40,22 +40,16 @@ export default function Portfolio(props) {
                     underline decoration-4 decoration-sky-500
                     pl-4 pr-5 mb-4 
                     rounded text-white`}>
-                            Convert Adobe XD to Website
+                            Politicians engagement ranking Project - In Development
                         </h4>
-                        <p className={`pl-4`}>Adobe XD converted and ready to deploy.</p>
-                        <Link href="https://danielniebraz.dev/" passHref>
+                        <p className={`pl-4`}>A digital platform to list the engagement of Brazilian politicians with unbiased and objective criteria, providing an overview of politicians on social networks.</p>
+                        <p className={`pl-4`}>NextJS + Tailwind</p>
+                        <Link href="https://ranking.danielniebraz.dev/" passHref>
                             <a className={`text-white hover:text-slate-50
                         hover:bg-slate-50/10
                         rounded-lg
                         cursor-pointer
-                        ring-1 p-2 my-2 mx-4 ring-slate-500 `} target="_blank">Adobe XD File</a>
-                        </Link>
-                        <Link href="https://themechasnft.com/" passHref>
-                            <a className={`text-white hover:text-slate-50
-                        hover:bg-slate-50/10
-                        rounded-lg
-                        cursor-pointer
-                        ring-1 p-2 my-2 mx-4 ring-slate-500`} target="_blank">Website</a>
+                        ring-1 p-2 my-2 mx-4 ring-slate-500 `} target="_blank">Project Site</a>
                         </Link>
                     </div>
 
@@ -64,51 +58,22 @@ export default function Portfolio(props) {
                     underline decoration-4 decoration-sky-500
                     pl-4 pr-5 mb-4 
                     rounded text-white`}>
-                            Frontend Vue Project
+                            SPCity App - Postgraduate Project
                         </h4>
-                        <p className={`pl-4`}>Site developed using Vue, TailwindCSS and Vuetify.</p>
-                        <Link href="https://vue.danielniebraz.dev/" passHref>
+                        <p className={`pl-4`}>An application to support the governance of cities, enabling citizens to point out problems and improvements more easily and conveniently.</p>
+                        <p className={`pl-4`}>Nextjs + Tailwind + What3words</p>
+                        <Link href="https://spcity.app/" passHref>
                             <a className={`text-white hover:text-slate-50
                         hover:bg-slate-50/10
                         rounded-lg
                         cursor-pointer
-                        ring-1 p-2 my-2 mx-4 ring-slate-500 `} target="_blank">Vue Project</a>
+                        ring-1 p-2 my-2 mx-4 ring-slate-500 `} target="_blank">SPCity Site</a>
                         </Link>
                     </div>
 
-                    <div className={`flex w-full md:w-1/6 flex-col`}>
-                        <h4 className={`text-base w-full text-left font-semibold 
-                    underline decoration-4 decoration-sky-500
-                    pl-4 pr-5 mb-4 
-                    rounded text-white`}>
-                            TailwindCSS Project
-                        </h4>
-                        <p className={`pl-4`}>Site developed with TailwindCSS.</p>
-                        <Link href="https://danielniebraz.dev/" passHref>
-                            <a className={`text-white hover:text-slate-50
-                        hover:bg-slate-50/10
-                        rounded-lg
-                        cursor-pointer
-                        ring-1 p-2 my-2 mx-4 ring-slate-500 `} target="_blank">TailwindCSS Project</a>
-                        </Link>
-                    </div>
+                    
 
-                    <div className={`flex w-full md:w-1/6 flex-col`}>
-                        <h4 className={`text-base w-full text-left font-semibold 
-                    underline decoration-4 decoration-sky-500
-                    pl-4 pr-5 mb-4 
-                    rounded text-white`}>
-                            Flutter Project
-                        </h4>
-                        <p className={`pl-4`}>Flutter Project.</p>
-                        <Link href="https://danielniebraz.dev/" passHref>
-                            <a className={`text-white hover:text-slate-50
-                        hover:bg-slate-50/10
-                        rounded-lg
-                        cursor-pointer
-                        ring-1 p-2 my-2 mx-4 ring-slate-500 `} target="_blank">Flutter Project</a>
-                        </Link>
-                    </div>
+                    
 
                 </div>
 
